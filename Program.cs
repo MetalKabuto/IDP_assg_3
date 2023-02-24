8f6b7610ea131f67a6dcbfbe416e54a6e7a8d839
MetalKabuto <metalamonite@gmail.com>
2023-02-24 12:15:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace D15_inlämning
+{
+    //Skrivet av Hannes Paulsson
+    internal class Program
+    {
+        //Uppgift 1.
+        public class Grundämne
+        {
+            public string namn;
+            // Z = atomnummer
+            public int Z;
+            public string typ;
+            // smält och kok mäts i kelvin
+            public double smältpunkt;
+            public double kokpunkt;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
